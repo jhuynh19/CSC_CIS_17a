7990e782f5a14f95beb636016b5601255212f941
John Huynh <jhuynh19@student.rccd.edu>
2025-09-07 19:57:35 -0700

    Add Review Homework 1: gaddis_9_2_13_divSales.cpp

---
+++ Hmwk/Review Homework 1/gaddis_9_2_13_divSales.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   gaddis_9_2_13_divSales.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on September 5th, 2025, 7:00 PM
+ * Purpose:  Write a cout statement so the variable divSales is displayed in a field of 8 spaces, in fixed-point notation, with a precision of 2 decimal places. The decimal point should always be displayed.
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+    
+    //Declare all variables for this function
+    
+    //Initialize all known variables
+    
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+    
+    //Display the Inputs/Outputs
+
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+    return 0;
+}
