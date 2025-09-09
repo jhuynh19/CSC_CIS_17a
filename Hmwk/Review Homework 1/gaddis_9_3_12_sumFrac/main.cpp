deecfcae27beef1930a22c0e8ae6ff3625c6bf38
John Huynh <jhuynh19@student.rccd.edu>
2025-09-09 11:48:11 -0700

    Add Review Homework 1: gaddis_9_3_12_sumFrac

--- Hmwk/Review Homework 1/gaddis_9_3_12_sumFrac/main.cpp
+++ Hmwk/Review Homework 1/gaddis_9_3_12_sumFrac/main.cpp
@@ -16,28 +16,37 @@ using namespace std;
 int main() {
 
     // Variables
-    float num1, num2, denom1, denom2, num_res, denom_res, result;
+    int num1, num2, denom1, denom2; 
+    float num_res, denom_res, result;
+    char choice;
 
     cout << "Let's add fractions together \n\n";
 
-    // Grab fractions from user
-    cout << "Numerator of first fraction \t";
-    cin >> num1;
-    cout << "Denominator of first fraction \t";
-    cin >> denom1;
-    cout << "Numerator of second fraction \t";
-    cin >> num2;
-    cout << "Denominator of second fraction \t";
-    cin >> denom2;
-    
-    // Calculate sum of two fractions
-    num_res = (num1 * denom2) + (num2 * denom1);
-    denom_res = denom1 * denom2;
-    result = num_res / denom_res;
-    
-    // Output result
-    cout << "\nResult as Fraction \t" << num_res << "/" << denom_res << endl;
-    cout << "Result as Decimal \t" << setprecision(4) << fixed << showpoint << result << endl;
+    do {
+        // Grab fractions from user
+        cout << "Numerator of first fraction \t";
+        cin >> num1;
+        cout << "Denominator of first fraction \t";
+        cin >> denom1;
+        cout << "Numerator of second fraction \t";
+        cin >> num2;
+        cout << "Denominator of second fraction \t";
+        cin >> denom2;
+        
+        // Calculate sum of two fractions
+        num_res = static_cast<int>(num1 * denom2) + (num2 * denom1);
+        denom_res = denom1 * denom2;
+        result = num_res / denom_res;
+        
+        // Output result
+        cout << "\nResult as Fraction \t" << num_res << "/" << denom_res << endl;
+        cout << "Result as Decimal \t" << setprecision(2) << fixed << showpoint << result << endl;
 
+        // Add more fractions
+        cout << "\nWant to add another fraction? (Y/N) \t";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+    
     return 0;
 }
