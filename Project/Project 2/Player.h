440e0ec7e3df760cc3e8c4750030b382a6b1c2b5
John <jhuynh19@student.rccd.edu>
2025-12-09 01:56:13 -0800

    Abstract base class with virtual interface

--- Project/Project 2/Player.h
+++ Project/Project 2/Player.h
@@ -14,4 +14,41 @@
 #include <vector>
 #include <string>
 
+class Player {
+    protected:
+
+        std::string name;           // Player name
+        Grid<int> myBoard;          // 0 = Empty, 1-5 = Ship IDs
+        Grid<char> enemyBoard;      // X = Hit, O = Miss
+        std::vector<Ship*> fleet;   // Dynamic list of ships
+        int shipsRemaining;         // Number of ships remaining
+        
+        // Mutators
+        void applyShipToBoard(Point p, bool vertical, int length, int shipId); // placeShip() function
+
+        // Accessors
+        bool canPlace(Point p, bool vertical, int length) const; // canPlace() function
+
+    public:
+        Player(std::string n);                                  // Sets name
+
+        virtual ~Player();                                      // Destructor
+        virtual void placeShips() = 0;                          // Player is manual, computer is random
+        virtual Point makeMove() = 0;                           // Player is manual, computer is random, then targeted
+
+        // Mutators
+        void initFleet(int mode);
+        void placeShipsRandomly();                              // Computer random fleet placement
+        bool receiveShot(Point p, bool &wasHit, bool &sunk);    // Process shot
+
+        // Getters
+        std::string getName() const { return name; }
+        int getBoardSize() const { return myBoard.getSize(); }  // Get board size
+        bool hasLost() const;                                   // Check for winner
+        
+        // Helpers
+        void printBoards() const;                               // Print board
+
+};
+
 #endif 
