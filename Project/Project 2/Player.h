508925db39420e3f1f2477ed7ea2767ad939935e
John <jhuynh19@student.rccd.edu>
2025-12-09 01:05:46 -0800

    Create .h files

---
+++ Project/Project 2/Player.h
@@ -0,0 +1,17 @@
+/* 
+ * File: Player.h
+ * Author: John Huynh
+ * Purpose: Abstract base class for the Battleship player
+ */
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "Grid.h" 
+#include "Ships.h"
+#include "Point.h"
+
+#include <vector>
+#include <string>
+
+#endif 
