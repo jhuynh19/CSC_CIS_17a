90aed392293527d75eba8de7a116d2560b3acf72
John <jhuynh19@student.rccd.edu>
2025-12-09 19:33:30 -0800

    Init setup of file

---
+++ Project/Project 2/Ships.h
@@ -0,0 +1,25 @@
+/* 
+ * File: Ships.h
+ * Author: John Huynh
+ * Purpose: Ship class and ship types
+ */
+
+#ifndef SHIPS_H
+#define SHIPS_H
+
+#include <string>
+#include <iostream>
+
+class Ship {
+    protected:
+    //
+
+    private:
+    //
+
+    public:
+    //
+
+};
+
+#endif
