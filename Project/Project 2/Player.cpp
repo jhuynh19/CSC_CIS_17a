94ad7e1bfeeb573de3fe65d423a7ecd5a402b326
John <jhuynh19@student.rccd.edu>
2025-12-11 00:19:49 -0800

    Recreatee initFleet and printBoards functions

--- Project/Project 2/Player.cpp
+++ Project/Project 2/Player.cpp
@@ -105,3 +105,64 @@ bool Player::receiveShot(Point p, bool &wasHit, bool &sunk) {
 bool Player::hasLost() const {
     return shipsRemaining == 0;
 }
+
+// Initialize Fleet
+void Player::initFleet(int mode) {
+    // Clear out any ships
+    for (int i = 0; i < fleet.size(); i++) {
+        delete fleet[i];
+    }
+    fleet.clear();
+
+    // Standard Mode
+    if (mode == 1) { 
+        fleet.push_back(new Carrier());
+        fleet.push_back(new Battleship());
+        fleet.push_back(new Cruiser());
+        fleet.push_back(new Submarine());
+        fleet.push_back(new Destroyer());
+    } 
+
+    // Rapid Mode
+    else { 
+        fleet.push_back(new Cruiser());
+        fleet.push_back(new Submarine());
+        fleet.push_back(new Destroyer());
+    }
+    
+    shipsRemaining = fleet.size();
+}
+
+// Print Boards to Console
+void Player::printBoards() const {
+    cout << "   Your Board             Enemy Board\n";
+    cout << "   ----------             -----------\n";
+    
+    int size = myBoard.getSize();
+    for (int r = 0; r < size; ++r) {
+
+        // Left Side is player board
+        cout << r << " |";
+        for (int c = 0; c < size; ++c) {
+            int id = myBoard(r, c);
+            char sym = '.';
+            
+            if (id > 0) {
+                Ship &s = *fleet[id - 1];
+                sym = s.getSymbol(); 
+            }
+            cout << " " << sym;
+        }
+        
+        cout << " |   "; 
+
+        // Right Side is computer's board
+        cout << r << " |";
+        for (int c = 0; c < size; ++c) {
+            char mark = enemyBoard(r, c);
+            if (mark == 0) mark = '.';
+            cout << " " << mark;
+        }
+        cout << " |\n";
+    }
+}
