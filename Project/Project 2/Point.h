7058a091188fe1f9f757cd4aecf9ff07320675a7
John <jhuynh19@student.rccd.edu>
2025-12-13 03:48:01 -0800

    Add Doxygen comments

--- Project/Project 2/Point.h
+++ Project/Project 2/Point.h
@@ -9,37 +9,55 @@
 
 #include <iostream>  
 
+/**
+ * @class Point
+ * @brief Represents a (row, column) coordinate on the game board.
+ */
 class Point {
-    protected:
-    // None needed
-    
-    private:
-    // None needed
-
     public:
-        int row;
-        int col;
+        int row; ///< Row index (0-9)
+        int col; ///< Column index (0-9)
 
-        // Helpers
-        static Point Quit() { return Point(-1, -1); }
-        bool isQuit() const { return row == -1 && col == -1; }
-
-        // Parameterized Constructor
+        /**
+         * @brief Constructor. Defaults to (0,0).
+         */
         Point(int r = 0, int c = 0) : row(r), col(c) {}
+
+        /**
+         * @brief Returns a special coordinate (-1, -1) signaling a Quit action.
+         */
+        static Point Quit() { return Point(-1, -1); }
+
+        /**
+         * @brief Checks if this point represents a Quit signal.
+         */
+        bool isQuit() const { return row == -1 && col == -1; }
     
-        // Operators
+        /**
+         * @brief Adds two points together (used for relative movement).
+         */
         Point operator+(const Point& right) const {                            
             return Point(row + right.row, col + right.col);
         }
 
+        /**
+         * @brief Equality check.
+         */
         bool operator==(const Point& right) const {
             return (row == right.row && col == right.col);                      
         }
 
+        /**
+         * @brief Inequality check.
+         */
         bool operator!=(const Point& right) const {                                         
             return !(*this == right);
         }
 
+        /**
+         * @brief Stream insertion operator.
+         * Prints coordinate in "A1" format (Column Letter + Row Number).
+         */
         friend std::ostream& operator<<(std::ostream& out, const Point& p) {    
             out << (char)('A' + p.col) << p.row;
             return out;
